15ef0cf46b539f15efefa6f2ce9d0318f28345aa
Dena Markudova <dena.markudova@gmail.com>
2021-06-18 17:47:16 +0200

    merge csv corrected

--- MergeCSV.py
+++ MergeCSV.py
@@ -6,7 +6,7 @@ def merge_csv(directory, time_aggregation):
     dataset_final = pd.DataFrame()
     for r, d, f in os.walk(directory):
         for file in f:
-            if f"_{time_aggregation}s.csv" in file:
+            if f"_{time_aggregation}ms.csv" in file:
                 df_app = pd.read_csv(os.path.join(r, file))
                 dataset_final = pd.concat([dataset_final, df_app], sort=False)
 
